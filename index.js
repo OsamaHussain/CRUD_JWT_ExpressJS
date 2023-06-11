fb76fab61d1ba69c77d4b8530e448edddad4025e
OsamaHussain <osamahussain897@gmail.com>
2023-06-12 04:59:28 +0500

    New Commit

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const userRouter = require("./route/userRoute");
 const app = express();
 require('dotenv').config({ path:'./config.env' });
 
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+app.use(cors(corsOptions)) // Use this after the variable declaration
+
 // Connection to Database;
 connectDB();
 
