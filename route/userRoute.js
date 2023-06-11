5123c94dcf41a8a2eefcd8f8da9d7bd83219f0bc
OsamaHussain <osamahussain897@gmail.com>
2023-06-12 03:33:37 +0500

    without jwt changes temporary

--- route/userRoute.js
+++ route/userRoute.js
@@ -10,24 +10,31 @@ router.post('/signup', signup);
 router.post('/login', login);
 
 // Fetch All The Users From Database
-router.get('/', verifyToken, getAllUsers);
+// router.get('/', verifyToken, getAllUsers);
+router.get('/', getAllUsers);
 
 // Fetch Specific User By Email
-router.get('/find', verifyToken, getUserByEmail);
+// router.get('/find', verifyToken, getUserByEmail);
+router.get('/find', getUserByEmail);
 
 // Fetch Specific User By ID
-router.get('/find/:id', verifyToken, getUserById);
+// router.get('/find/:id', verifyToken, getUserById);
+router.get('/find/:id', getUserById);
 
 // Update a User By Email
-router.get('/update', verifyToken, updateUserByEmail);
+// router.get('/update', verifyToken, updateUserByEmail);
+router.get('/update', updateUserByEmail);
 
 // Update a User By ID
-router.get('/update/:id', verifyToken, updateUserById);
+// router.get('/update/:id', verifyToken, updateUserById);
+router.get('/update/:id', updateUserById);
 
 // Delete a User By Email
-router.get('/delete', verifyToken, deleteUserByEmail);
+// router.get('/delete', verifyToken, deleteUserByEmail);
+router.get('/delete', deleteUserByEmail);
 
 // Delete a User By ID
-router.get('/delete/:id', verifyToken, deleteUserById);
+// router.get('/delete/:id', verifyToken, deleteUserById);
+router.get('/delete/:id', deleteUserById);
 
 module.exports = router;
